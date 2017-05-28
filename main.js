1c74f28d20818458d9f93c48eb52bb2a934a0ca4
Kchalmers <kevinqchal@yahoo.com>
2017-05-27 22:23:18 -0700

    fully functional

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 var PokemonSearcher = function() {
     var self = this;
     var listUrl = "http://pokeapi.co/api/v2/pokemon/";
+    var pokemonCount = 0;
     this.pokemonFinder = function () {
         $.ajax({
             dataType: "JSON",
             method: "GET",
             url: listUrl,
             success: function (response) {
-                var pokemonInfo = "Could not find pokemon";
+                var pokemonInfo = "Could not find the pokemon. Try again.";
                 for(var i = 0; i < response.results.length; i++){
-                    if(response.results[i].name === $("#searchInput").val()){
+                    pokemonCount++;
+                    if(response.results[i].name === $("#searchInput").val().toLowerCase()){
                         pokemonInfo = response.results[i];
+                        self.clearSearch();
                         self.getPokemonData(pokemonInfo.url);
                         break;
                     }
                     else if (i === response.results.length -1 && response.next){
                         listUrl = response.next;
-                        console.log(listUrl);
                         self.pokemonFinder();
+                    }
+                    else if(pokemonCount === response.count){
+                        self.clearSearch();
+                        $("#display").append($("<div>").text(pokemonInfo).addClass("pokemonProfile"));
                     }
                 }
             },
@@ -34,15 +40,34 @@ var PokemonSearcher = function() {
         });
     };
     this.displayPokemon = function (pokemon) {
+        console.log(pokemon);
         var pokemonProfile= $("<div>").addClass("pokemonProfile");
         var pokemonPictureHolder = $("<div>").addClass("imgContainer");
         var pokemonPicture = $("<img>").addClass("img-fluid");
-        pokemonPicture.attr("src", pokemon.sprites.front_default).addClass("animalPicture");
-        pokemonPictureHolder.append(pokemonPicture);
+        if(pokemon.sprites.front_default) {
+            pokemonPicture.attr("src", pokemon.sprites.front_default).addClass("pokemonPicture");
+            pokemonPictureHolder.append(pokemonPicture);
+        }
         var pokemonName = $("<div>").text(pokemon.name).addClass('pokemonName');
-        pokemonProfile.append(pokemonPictureHolder, pokemonName);
+        var pokemonHeight = $("<div>").text("Height: " + pokemon.height).addClass('pokemonHeight');
+        var pokemonWeight = $("<div>").text("Weight: " + pokemon.weight).addClass('pokemonWeight');
+        var pokemonStats = $("<div>").text("Stats: ").addClass("pokemonStats");
+        for(var i = pokemon.stats.length -1; i >= 0; i--){
+            pokemonStats.append($("<div>").text(pokemon.stats[i].stat.name + ': ' + pokemon.stats[i].base_stat));
+        }
+        var pokemonTypes = $("<div>").text("Type: ").addClass("pokemonType");
+        for(var j = 0; j < pokemon.types.length; j++){
+            pokemonTypes.append(pokemon.types[j].type.name);
+        }
+        pokemonProfile.append(pokemonName, pokemonPictureHolder, pokemonTypes, pokemonHeight, pokemonWeight, pokemonStats);
         $("#display").append(pokemonProfile);
     };
+    this.clearSearch = function () {
+        $("#searchInput").val('');
+        $("#display").empty();
+        pokemonCount = 0;
+        listUrl = "http://pokeapi.co/api/v2/pokemon/";
+    }
 };
 
 $(document).ready(function() {
